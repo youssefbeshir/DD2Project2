fffd98262a083f05d4d2594936011ecc7cc7bb6d
Mohamed Ashraf <75078872+mohammedashraftaha@users.noreply.github.com>
2020-11-26 16:18:08 +0200

    Add files via upload

--- clk_gating_utility.py
+++ clk_gating_utility.py
@@ -9,22 +9,36 @@ statements = []
 rtl = sys.argv[1] #give the rtl file as an input in the command line
 ast, _ = parse([rtl])
 
+desc = ast.description # get the root node of the tree (Description)
+definition = desc.definitions[0] # get the ModuleDef node
 
-# get the root node of the tree (Description)
-desc = ast.description
+Newitems = []
+flag = True
 
-# get the ModuleDef node
-definition = desc.definitions[0]
+cg_out = vast.Wire('cg_out')
+Newitems.append(cg_out)
+
+
+for itemDeclaration in definition.items:
+    flag = True
+    item_type = type(itemDeclaration).__name__
+    if item_type == "InstanceList":
+        instance = itemDeclaration.instances[0] 
+        if instance.module == "sky130_fd_sc_hd__mux2_1":
+          	flag = False
+        for hook in instance.portlist:
+            if hook.portname == "D":
+                hook.argname = vast.Identifier("cg_out") 
+    if flag == True:
+       
+       Newitems.append(itemDeclaration)
+   	
+
 
 #add clock gate cell sky130_fd_sc_hd_dlclkp
 #output GCLK
 #input GATE
 #input CLK
-
-instances = [] # this is an empty list to add the new instances in
-cg_out = vast.Wire('cg_out')
-
-
 clock_gate_args= [
         vast.PortArg("GCLK" ,vast.Identifier("cg_out")),
         vast.PortArg("GATE",vast.Identifier("en")),
@@ -39,30 +53,13 @@ clock_gate_cell = vast.Instance(
         tuple()                            
 )
 
-clock_gate_args= [
-        vast.PortArg("GCLK" ,vast.Identifier("cg_out")),
-        vast.PortArg("GATE",vast.Identifier("en")),
-        vast.PortArg("CLK",vast.Identifier("clk"))
-]
-
-
-clock_gate_cell = vast.Instance(
-        "sky130_fd_sc_hd_dlclkp",
-        "__clock_gate_cell__2",
-        tuple(clock_gate_args),
-        tuple()                            
-)
-
-
-instances.append(vast.InstanceList("sky130_fd_sc_hd__dlclkp", tuple(), tuple([clock_gate_cell])))
-
+Newitems.append(vast.InstanceList("sky130_fd_sc_hd__dlclkp", tuple(), tuple([clock_gate_cell])))
 
 
 # Add the instances list to the AST items
-items = list(definition.items)
-items = items + instances 
+items = Newitems
+items = items 
 
-items.append(cg_out)
 
 definition.items = tuple(items)
 
@@ -74,3 +71,4 @@ rslt = codegen.visit(ast)
 f = open("clkgatingafter.gl.v", "w+")
 f.write(rslt)
 f.close()
+
---
+++ clkgatingafter.gl.v
@@ -0,0 +1,37 @@
+
+
+module clkgatingbefore
+(
+  d_in,
+  clk,
+  en,
+  d_out
+);
+
+  wire cg_out;
+  wire _0_;
+  input clk;
+  input d_in;
+  output d_out;
+  input en;
+
+  sky130_fd_sc_hd__dfxtp_1
+  _2_
+  (
+    .CLK(clk),
+    .D(cg_out),
+    .Q(d_out)
+  );
+
+
+  sky130_fd_sc_hd__dlclkp
+  __clock_gate_cell__
+  (
+    .GCLK(cg_out),
+    .GATE(en),
+    .CLK(clk)
+  );
+
+
+endmodule
+
